613ca30449586fc1f834cd8ab730ab5bdf32e5d6
atinder <atinder.singh@srijan.net>
2021-08-12 11:50:06 +0530

    ios updtaed

--- ios/RNReactNativeImmediatePhoneCallSrijan.h
+++ ios/RNReactNativeImmediatePhoneCallSrijan.h
@@ -1,11 +1,7 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#import <UIKit/UIKit.h>
 #import <React/RCTBridgeModule.h>
-#endif
+#import <React/RCTBridge.h>
 
 @interface RNReactNativeImmediatePhoneCallSrijan : NSObject <RCTBridgeModule>
 
 @end
-  
--- ios/RNReactNativeImmediatePhoneCallSrijan.m
+++ ios/RNReactNativeImmediatePhoneCallSrijan.m
@@ -1,20 +1,15 @@
-
+#import <UIKit/UIKit.h>
 #import "RNReactNativeImmediatePhoneCallSrijan.h"
 
 @implementation RNReactNativeImmediatePhoneCallSrijan
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(immediatePhoneCall:(NSString *)number)
 {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel:%@", number]]];
-    });
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel:%@", number]]];
 };
 
 @end
+  
   
